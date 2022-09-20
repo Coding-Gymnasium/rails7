82aa3e536fc8ddeab1e67a85e8d68489b7446a73
Nico Rithner <nicorithner@gmail.com>
2022-09-20 09:33:25 -0600

    Update tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,17 @@ class TasksController < ApplicationController
     @tasks = ['Fold laundry', 'Sweep porch', 'Wash dishes', 'Mow lawn']
   end
 
+  def show; end
+
   def new; end
 
+  def create; end
+
   def edit; end
+
+  def update; end
+
+  def delete; end
+
+  def destroy; end
 end
